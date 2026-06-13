51a70f4a414384190a0e04d94684f4868b7ddf35
SALONI JOSHI <saloni408joshi@gmail.com>
2026-06-13 21:10:11 +0530

    Completed Task 2 - Contact Form, Validation, Responsive Layout and To-Do List

--- script.js
+++ script.js
@@ -1,3 +1,45 @@
-function showMessage(){
-    alert("Welcome to my ApexPlanet Internship Project!");
+const form = document.getElementById("contactForm");
+
+form.addEventListener("submit",function(e){
+
+e.preventDefault();
+
+let name=document.getElementById("name").value;
+
+let email=document.getElementById("email").value;
+
+let message=document.getElementById("message");
+
+if(name==="" || email===""){
+message.innerText="Please fill all fields";
+return;
+}
+
+if(!email.includes("@")){
+message.innerText="Invalid Email";
+return;
+}
+
+message.innerText="Form Submitted Successfully";
+
+});
+
+function addTask(){
+
+let input=document.getElementById("taskInput");
+
+let task=input.value;
+
+if(task===""){
+return;
+}
+
+let li=document.createElement("li");
+
+li.innerText=task;
+
+document.getElementById("taskList").appendChild(li);
+
+input.value="";
+
 }
